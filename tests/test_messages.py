4536b9358950c9c738dd7e7d8ef1f65a89903427
Ryan Lerch <rlerch@redhat.com>
2024-04-09 20:06:52 +1000

    add message schema
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

---
+++ tests/test_messages.py
@@ -0,0 +1,88 @@
+# SPDX-FileCopyrightText: 2024 Contributors to the Fedora Project
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+"""Unit tests for the message schema."""
+
+import pytest
+from jsonschema import ValidationError
+
+from tahrir_messages import BadgeAwardV1, PersonLoginFirstV1, PersonRankAdvanceV1
+
+from .utils import DUMMY_BADGE, DUMMY_PERSON, DUMMY_USER
+
+
+def test_person_login_first():
+    body = {
+        "user": DUMMY_USER,
+    }
+    message = PersonLoginFirstV1(body=body)
+    message.validate()
+
+    expected_summary = "dudemcpants logged into badges for the first time"
+    assert message.summary == expected_summary
+    assert str(message) == expected_summary
+    assert message.agent_name == "dudemcpants"
+    assert message.usernames == ["dudemcpants"]
+    assert message.app_name == "tahrir"
+    assert message.url is None
+    assert message.app_icon == "https://apps.fedoraproject.org/img/icons/badges.png"
+
+
+def test_person_login_first_missing_fields():
+    message = PersonLoginFirstV1(body={})
+    with pytest.raises(ValidationError):
+        message.validate()
+
+
+def test_badge_award():
+    body = {"user": DUMMY_USER, "badge": DUMMY_BADGE}
+    message = BadgeAwardV1(body=body)
+    message.validate()
+
+    expected_summary = "dudemcpants was awarded the badge `White Hat`"
+    assert message.summary == expected_summary
+    assert str(message) == expected_summary
+    assert message.agent_name == "dudemcpants"
+    assert message.usernames == ["dudemcpants"]
+    assert message.app_name == "tahrir"
+    assert message.url is None
+    assert message.app_icon == "https://apps.fedoraproject.org/img/icons/badges.png"
+
+
+def test_badge_award_missing_fields():
+    message = BadgeAwardV1(body={})
+    with pytest.raises(ValidationError):
+        message.validate()
+
+    body = {"badge": DUMMY_BADGE}
+    message = BadgeAwardV1(body=body)
+    with pytest.raises(ValidationError):
+        message.validate()
+
+    body = {"user": DUMMY_BADGE}
+    message = BadgeAwardV1(body=body)
+    with pytest.raises(ValidationError):
+        message.validate()
+
+
+def test_person_rank_advance():
+    body = {"old_rank": 2, "person": DUMMY_PERSON}
+    message = PersonRankAdvanceV1(body=body)
+    message.validate()
+
+    expected_summary = "aaronhale's Badges rank changed from 2 to 1"
+    assert message.summary == expected_summary
+    assert str(message) == expected_summary
+    assert message.agent_name == "aaronhale"
+    assert message.usernames == ["aaronhale"]
+    assert message.app_name == "tahrir"
+    assert message.url is None
+    assert message.app_icon == "https://apps.fedoraproject.org/img/icons/badges.png"
+
+
+def test_person_rank_advance_missing_fields():
+    """Assert an exception is actually raised on validation failure."""
+    message = BadgeAwardV1(body={})
+    with pytest.raises(ValidationError):
+        message.validate()
--- tests/utils.py
+++ tests/utils.py
@@ -5,7 +5,23 @@
 """Utilities for the unit tests."""
 
 
-DUMMY_THING = {
-    "id": 1,
-    "name": "dummy",
+DUMMY_USER = {
+    "username": "dudemcpants",
+    "badges_user_id": 123456,
+}
+
+DUMMY_BADGE = {
+    "badge_id": "white-hat",
+    "description": "You submitted a bodhi update with type security",
+    "image_url": "https://badges.fedoraproject.org/pngs/white-hat.png",
+    "name": "White Hat",
+}
+
+DUMMY_PERSON = {
+    "bio": None,
+    "email": "aaronhale@fedoraproject.org",
+    "id": 1234,
+    "nickname": "aaronhale",
+    "rank": 1,
+    "website": None,
 }
